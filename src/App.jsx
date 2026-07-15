90906b2b49e57009262fb82ca6d72115d91047cd
alstj9653 <minseo_good@naver.com>
2026-07-15 17:28:05 +0900

    폴더 재구성

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import Hero from "./components/Hero";
-import Direction from "./components/Direction";
-import Program from "./components/Program";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import Home from "./pages/Home.jsx";
+import Project from "./pages/Project.jsx";
+import TeamPage from "./components/TeamPage.jsx";
 
-function App() {
+export default function App() {
   return (
     <>
-      <Hero />
-      <Direction />
-      <Program />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/project" element={<Project />} />
+        <Route
+          path="/team"
+          element={
+            <div className="lk-page" id="top">
+              <TeamPage />
+            </div>
+          }
+        />
+      </Routes>
+      <Footer />
     </>
   );
 }
-
-export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import 'pretendard/dist/web/static/pretendard.css'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
-)
+);
